c286955e62b2c369a81ad239d6d0b0818446ec5b
Rain-Hub1 <rainhubsla@gmail.com>
2025-12-09 20:25:03 -0300

    Update Source.lua

--- Source.lua
+++ Source.lua
@@ -1,3 +1,30 @@
 local Lib = {}
 
+function new(c, p)
+ local k = Instance.new(c)
+  for pp, v in pairs(p or {}) do
+   k[pp] = v
+  end
+ return k
+end
+
+function Lib:Window(Info)
+ local Info = Info or {}
+ local CoreGui = game:GetService("CoreGui")
+ local s = new("ScreenGui", {
+  Parent = CoreGui
+ })
+ 
+ local Win
+ 
+ local Tbar
+ 
+ local Title
+end
+
+function Lib:Demo()
+ Lib:Window()
+end
+Lib:Demo()
+
 return Lib
