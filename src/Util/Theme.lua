863acc476afbb92fafb05398d68944323afab5ba
Rain-Hub1 <rainhubsla@gmail.com>
2025-12-15 00:45:28 -0300

    Create Theme.lua

---
+++ src/Util/Theme.lua
@@ -0,0 +1,8 @@
+return {
+    UIStroke = Color3.fromHex("#1D3339"),
+    Frame    = Color3.fromHex("#11181A"),
+    Button   = Color3.fromHex("#00BFFF"),
+    Title    = Color3.fromHex("#00E5FF"),
+    Name     = Color3.fromHex("#B3D1D9"),
+    Desc     = Color3.fromHex("#5B7A84")
+}
