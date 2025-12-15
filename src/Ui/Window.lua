546cd19352b914deb7637952d0a60a2ca6ac8583
Rain-Hub1 <rainhubsla@gmail.com>
2025-12-15 00:32:43 -0300

    Update Window.lua

--- src/Ui/Window.lua
+++ src/Ui/Window.lua
@@ -1,6 +1,8 @@
 local Win = {}
-  function Win:Print(Info)
-    local Info = Info or {}
-    print(Info.Text or "Ola!")
-  end
+
+function Win:Print(info)
+  info = info or {}
+  print(info.Text or "Ola!")
+end
+
 return Win
