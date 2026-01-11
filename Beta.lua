dbd997197f9c859de956022b848d254590208063
Rain-Hub1 <rainhubsla@gmail.com>
2026-01-11 16:36:37 -0300

    Update Beta.lua

--- Beta.lua
+++ Beta.lua
@@ -1,1 +1,15 @@
-local Hi = "!!!!"
+local Lib = {}
+
+function new(c, p)
+  local k = Instance.new(c)
+  for pp, v in pairs(p or {}) do
+    k[pp] = v
+  end
+  return k
+end
+
+function Lib:Window(Info)
+  local Info = Info or {}
+end
+
+return Lib
