49f8c2f524160c8591fdf5880e5defa677f41126
Rain-Hub1 <rainhubsla@gmail.com>
2026-01-13 16:12:47 -0300

    Update Beta.lua

--- Beta.lua
+++ Beta.lua
@@ -8,6 +8,34 @@ function new(c, p)
   return k
 end
 
+local UserInputService = game:GetService("UserInputService")
+local player = game:GetService("Players").LocalPlayer
+
+function MDrag(Area, Parent)
+  local isDragging = false
+  local dragStart
+  local startPos
+  Area.InputBegan:Connect(function(input)
+    if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
+      isDragging = true
+      dragStart = input.Position
+      startPos = Parent.Position
+      return Enum.UserInputState.Begin
+    end
+  end)
+  Area.InputEnded:Connect(function(input)
+    if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
+      isDragging = false
+    end
+  end)
+  UserInputService.InputChanged:Connect(function(input)
+    if isDragging and (input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch) then
+      local delta = input.Position - dragStart
+      Parent.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+    end
+  end)
+end
+
 local Def = {
   ["T"] = {
     ["Dark"] = {
@@ -21,8 +49,12 @@ local Def = {
   },
   ["..."] = false
 }
+
 function Lib:Window(Info)
   local Info = Info or {}
+  local s
+  local Win
+  local Tbar
 end
 
 return Lib
